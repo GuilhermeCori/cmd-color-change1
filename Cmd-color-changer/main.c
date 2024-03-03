28cf41490547468eca6dadee8e38c0a9b0db088e
GuilhermeCori <161992901+GuilhermeCori@users.noreply.github.com>
2024-03-03 00:23:50 -0300

    Add files via upload

---
+++ Cmd-color-changer/main.c
@@ -0,0 +1,269 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    int Numero;
+    char letra;
+    setlocale(LC_ALL,"Portuguese");
+    printf("Mude a cor do Prompt de Comando\n");
+    printf("Para isso, Você precisa digitar dois caracteres, sendo o primeiro um números e o segundo uma letra,conforme o exemplo a seguir\n1,A\n");
+    printf("Os números podem ser de 0 a 9 e, as letras de A a E\n");
+    printf("Digite um número\n");
+    scanf("%d",&Numero);
+            if(Numero==1){
+                system("color 01");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 1A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 1B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 1C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 1D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 1E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 1F");
+                      }
+
+            }
+            if(Numero==2){
+                system("color 02");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 2A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 2B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 2C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 2D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 2E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 2F");
+                      }
+
+            }
+            if(Numero==3){
+                system("color 03");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 3A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 3B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 3C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 3D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 3E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 3F");
+                      }
+
+            }
+            if(Numero==4){
+                system("color 04");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 4A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 4B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 4C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 4D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 4E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 4F");
+                      }
+
+            }
+            if(Numero==5){
+                system("color 05");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 5A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 5B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 5C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 5D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 5E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 5F");
+                      }
+
+            }
+            if(Numero==6){
+                system("color 06");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 6A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 6B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 6C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 6D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 6E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 6F");
+                      }
+
+            }
+            if(Numero==7){
+                system("color 07");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 7");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 7B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 7C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 7D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 7E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 7F");
+                      }
+
+            }
+            if(Numero==8){
+                system("color 08");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 8A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 8B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 8C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 8D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 8E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 8F");
+                      }
+
+            }
+            if(Numero==9){
+                system("color 09");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 9A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 9B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 9C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 9D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 9E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 9F");
+                      }
+
+            }
+            if(Numero==0){
+                system("color 00");
+                printf("Agora,Digite uma letra\n");
+                fflush(stdin);
+                scanf("%c",&letra);
+                      if(letra=='A'||letra=='a'){
+                        system("color 0A");
+                      }
+                      if(letra=='B'||letra=='b'){
+                        system("color 0B");
+                      }
+                      if(letra=='C'||letra=='c'){
+                        system("color 0C");
+                      }
+                      if(letra=='D'||letra=='d'){
+                        system("color 0D");
+                      }
+                      if(letra=='E'||letra=='e'){
+                        system("color 0E");
+                      }
+                      if(letra=='F'||letra=='f'){
+                        system("color 0F");
+                      }
+
+            }
+            printf("Mudou de cor XDDD");
+
+
+}
+
